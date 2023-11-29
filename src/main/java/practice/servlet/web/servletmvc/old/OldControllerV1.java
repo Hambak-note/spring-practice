0a73772cfa26144503f795c60279b61fee97a419
Hambak-note <dudghks5722@gmail.com>
2023-11-29 23:31:06 +0900

    [fix] 버전으로 분리하여 구별하도록 변경

--- src/main/java/practice/servlet/web/servletmvc/old/OldControllerV1.java
+++ src/main/java/practice/servlet/web/servletmvc/old/OldControllerV1.java
@@ -7,13 +7,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-@Component("/springmvc/old-controller")
-public class OldController implements Controller {
+@Component("/springmvc/old-controllerV1")
+public class OldControllerV1 implements Controller {
 
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return new ModelAndView("new-form");
+        return null;
     }
 }
