209d6ef5a64e3f999cf3198d95e5cb6532a436bf
Hambak-note <dudghks5722@gmail.com>
2023-11-29 23:56:20 +0900

    [feat] 이전까지 만들었던 컨트롤러를 @RequestMapping 기반으로 구현

--- src/main/java/practice/servlet/ServletApplication.java
+++ src/main/java/practice/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package practice.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import practice.servlet.web.servletmvc.v1.SpringMemberFormControllerV1;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
