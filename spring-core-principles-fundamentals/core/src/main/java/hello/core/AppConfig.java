ab7ac4db8f8f038bee13ca42ac98a3df4eefc121
Hambak-note <dudghks5722@gmail.com>
2023-12-19 19:35:33 +0900

    feat: 기존 자바 코드에서 스프링을 사용하도록 변경
    DI를 스프링 컨테이너가 수행할 수 있도록 변경하고 실행 클래스에서 이를 적용

--- spring-core-principles-fundamentals/core/src/main/java/hello/core/AppConfig.java
+++ spring-core-principles-fundamentals/core/src/main/java/hello/core/AppConfig.java
@@ -7,13 +7,18 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderServiceImpl orderService() {
         return new OrderServiceImpl(
                memberRepository(),
@@ -21,10 +26,12 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
