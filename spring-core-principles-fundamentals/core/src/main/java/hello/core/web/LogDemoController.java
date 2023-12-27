0565c793a97cdc39085ceb8f0a6e30168a4f8cf4
Hambak-note <dudghks5722@gmail.com>
2023-12-27 11:20:32 +0900

    feat: 프록시를 사용해 request 스코프 실습 코드

--- spring-core-principles-fundamentals/core/src/main/java/hello/core/web/LogDemoController.java
+++ spring-core-principles-fundamentals/core/src/main/java/hello/core/web/LogDemoController.java
@@ -5,7 +5,6 @@ import hello.core.common.MyLogger;
 import hello.core.logdemo.LogDemoService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,17 +14,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
+
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
+
+        System.out.println("myLogger = " + myLogger.getClass());
         return "OK";
     }
 }
