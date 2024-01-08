d54f6428a6a8c5345a1259df4d2fecd718afa3ab
Hambak-note <dudghks5722@gmail.com>
2024-01-08 18:38:49 +0900

    feat: 로그를 출력하기 위한 어노테이션 추가

--- spring-db-vol2/itemservice/src/main/java/hello/itemservice/repository/mybatis/MyBatisItemRepository.java
+++ spring-db-vol2/itemservice/src/main/java/hello/itemservice/repository/mybatis/MyBatisItemRepository.java
@@ -5,11 +5,13 @@ import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class MyBatisItemRepository implements ItemRepository {
