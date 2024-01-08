b2ac54802a784b9e2c405de5370810626b730d19
Hambak-note <dudghks5722@gmail.com>
2024-01-08 17:25:41 +0900

    feat: 이름 지정 파라미터를 사용하도록 repository 코드를 수정, 이에 따른 config 파일 생성

--- spring-db-vol2/itemservice/src/main/java/hello/itemservice/config/JdbcTemplateV2Config.java
+++ spring-db-vol2/itemservice/src/main/java/hello/itemservice/config/JdbcTemplateV2Config.java
@@ -2,6 +2,7 @@ package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV1;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV2;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
 import lombok.RequiredArgsConstructor;
@@ -10,19 +11,20 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class JdbcTemplateV1Config {
+public class JdbcTemplateV2Config {
 
     private final DataSource dataSource;
 
     @Bean
     public ItemService itemService() {
+
         return new ItemServiceV1(itemRepository());
     }
 
     @Bean
     public ItemRepository itemRepository() {
-        return new JdbcTemplateItemRepositoryV1(dataSource);
+        return new JdbcTemplateItemRepositoryV2(dataSource);
     }
 }
