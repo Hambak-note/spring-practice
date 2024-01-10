67a4d0aae4368f1a9128e95f5a1e284028abebf6
Hambak-note <dudghks5722@gmail.com>
2024-01-10 20:55:15 +0900

    feat: 예제 프로젝트 V2_인터페이스 없는 구체 클래스를 만들고 스프링 빈을 수동으로 등록

---
+++ spring-core-principles-advanced/proxy/src/main/java/hello/proxy/app/v2/OrderServiceV2.java
@@ -0,0 +1,17 @@
+package hello.proxy.app.v2;
+
+import hello.proxy.app.v1.OrderServiceV1;
+
+public class OrderServiceV2 implements OrderServiceV1 {
+
+    private final OrderRepositoryV2 orderRepository;
+
+    public OrderServiceV2(OrderRepositoryV2 orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+}
